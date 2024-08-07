32e633d40aa5f861e2c4c4d7b4c99aa0b6e823eb
Leszek02 <leszek.miernicki02@gmail.com>
2024-08-07 14:26:51 +0200

    transfer to a new repo

---
+++ Compress.js
@@ -0,0 +1,104 @@
+const fs = require('fs');
+const env = require('node:process');
+
+require('dotenv').config();
+
+const gameTemplate = {
+    White: "username1",
+    Black: "username2",
+    UTCDate: "2024.13.13 25:61:61?",
+    WhiteElo: "4000",
+    BlackElo: "-4000",
+    TimeControl: "0-30",
+    Termination: "yes",
+    Link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    gameConstructor: function(white, black, UTCDate, WhiteElo, BlackElo, TimeControl, Termination, Link) {
+            this.White = white,
+            this.Black = black,
+            this.UTCDate = UTCDate,
+            this.WhiteElo = WhiteElo,
+            this.BlackElo = BlackElo,
+            this.TimeControl = TimeControl,
+            this.Termination = Termination,
+            this.Link = Link}}
+
+
+async function findGame(gameString) {
+  game = '';
+  await downloadGames().then((pgn) => {
+    const dataLines = pgn.split('\n');
+    for (let i = 20; i < dataLines.length; i = i + 25) {
+      if (dataLines[i].startsWith(`[Link "${gameString}`)) {
+        for (let j = i - 20; j < i + 3; j++) {
+          game += dataLines[j] + '\n';
+        }
+        break;
+      }
+    }
+  });
+  return game;
+}
+
+
+async function downloadGames() {
+  let curTime = new Date();
+  let year = curTime.getFullYear();
+  let month = curTime.getMonth() + 1;
+  api = `https://api.chess.com/pub/player/${process.env.CHESSCOM_USERNAME}/games/${year}/${month}/pgn`;
+  let response = await fetch(api, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/x-chess-pgn',
+    },
+  });
+  const pgn = await response.text();
+  return pgn;
+}
+
+
+function extract(str) {
+    const match = str.match(/"([^"]*)"/);
+    return match ? match[1] : null;
+}
+
+
+function convertToDate(dateString, timeString) {
+  const [year, month, day] = dateString.split('.').map(Number);
+  const [hour, minute, seconds] = timeString.split(':').map(Number);
+  return new Date(Date.UTC(year, month - 1, day, hour, minute, seconds));
+}
+
+
+async function newestGames(newestNumber){
+    gameList = [];
+    await downloadGames().then((pgn) => {
+        const dataLines = pgn.split('\n');
+        for (let i = 22; i < dataLines.length; i = i + 25) {
+            const newGame = Object.create(gameTemplate);
+            
+            newGame.gameConstructor( 
+                extract(dataLines[i - 18]),
+                extract(dataLines[i - 17]),
+                convertToDate(extract(dataLines[i - 11]), extract(dataLines[i - 10])),
+                extract(dataLines[i - 9]),
+                extract(dataLines[i - 8]),
+                extract(dataLines[i - 7]),
+                extract(dataLines[i - 6]),
+                extract(dataLines[i - 2])
+            )
+            if (i == 22) {
+                //console.log("%o", newGame);
+            }
+            gameList.push(newGame);
+        }
+    })
+    gameList.sort((a, b) => new Date(b.UTCDate) - new Date(a.UTCDate));
+    return gameList.slice(0, newestNumber);
+}
+
+
+// newestGames(5).then((gameList) => {
+//     console.log(gameList);
+// })
+
+module.exports = { findGame };
---
+++ Main.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const bodyParser = require('body-parser');
+const path = require('path');
+const fs = require('fs');
+
+const { findGame }  = require('./Compress.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.text());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/submit', async (req, res) => {
+  const content  = await req.body;
+  let pgn = await findGame(content);
+
+  startBot(pgn);
+});
+
+async function startBot(pgn) {
+  try {
+    const browser = await puppeteer.launch({
+      headless: false,
+      args: ['--start-maximized'],
+    });
+    const page = await browser.newPage();
+    const { width, height } = await page.evaluate(() => {
+      return {
+        width: window.screen.width,
+        height: window.screen.height - 80,
+      };
+    });
+    await page.setViewport({ width, height });
+
+    // Load session data
+    const sessionFile = 'session.json';
+    if (fs.existsSync(sessionFile)) {
+      const sessionData = JSON.parse(fs.readFileSync(sessionFile));
+      await page.setCookie(...sessionData.cookies);
+      await page.evaluateOnNewDocument((storageData) => {
+        localStorage.clear();
+        for (const key in storageData) {
+          localStorage.setItem(key, storageData[key]);
+        }
+      }, sessionData.localStorage);
+    }
+
+    await page.goto('https://lichess.org/paste');
+
+    // Check if already logged in
+    const isLoggedIn = await page.evaluate(() => {
+      return !!!document.querySelector('.signin');
+    });
+
+    if (!isLoggedIn) {
+      // Perform login
+      await page.goto('https://lichess.org/login');
+      await page.type('input[name="username"]', process.env.LICHESS_USERNAME);
+      await page.type('input[name="password"]', process.env.LICHESS_PASSWORD);
+      await page.click('.submit.button');
+      await page.waitForNavigation();
+      await page.goto('https://lichess.org/paste', { waitUntil: 'domcontentloaded' });
+
+      //Save session data
+      const cookies = await page.cookies();
+      const localStorage = await page.evaluate(() => {
+        let data = {};
+        for (let i = 0; i < localStorage.length; i++) {
+          const key = localStorage.key(i);
+          data[key] = localStorage.getItem(key);
+        }
+        return data;
+      });
+
+      fs.writeFileSync(sessionFile, JSON.stringify({ cookies, localStorage }, null, 2));
+
+      await new Promise((resolve) => setTimeout(resolve, 200));
+
+    }
+
+    await page.waitForSelector('#form3-pgn');
+
+    await page.evaluate((text) => {
+      document.querySelector('#form3-pgn').value = text;
+    }, pgn);
+
+    await page.click('label[for="form3-analyse"]');
+    await page.click('.submit.button.text');
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
